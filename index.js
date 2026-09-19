be17d933ed3c15adabd45688a28362fc019ff7b9
Nandinigit26 <nandiniy2026@gmail.com>
2026-09-19 13:33:18 +0530

    modified the Js file

--- index.js
+++ index.js
@@ -10,9 +10,4 @@ function greet(person) {
 
 console.log(greet('Alice'));
 
-const numbers = [1, 2, 3, 4, 5];
-const doubled = numbers.map((num) => num * 2);
-console.log('Doubled numbers:', doubled);
-
-const total = numbers.reduce((sum, num) => sum + num, 0);
-console.log('Total:', total);
+console.log(greeat('Bob'));
